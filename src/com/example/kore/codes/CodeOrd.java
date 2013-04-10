d1d9106a254725ea507282b589c13bd47dfe357b
andrew-miller <andrew-miller>
2013-04-10 19:45:13 -0400

    work in progress: trying to make a proper order for Code...
    but ran into issue: fj.data.TreeMap doesn't provide a way to iterate the keys in order.
    can't ask on the functional java site (on google code) because the registration is broken (blocks IP or requires phone number, who the fuck knows)

--- src/com/example/kore/codes/CodeOrd.java
+++ src/com/example/kore/codes/CodeOrd.java
@@ -1,20 +1,74 @@
 package com.example.kore.codes;
 
-import fj.F2;
-import fj.Ord;
-import fj.Ordering;
+import java.io.Serializable;
 
+import com.example.unsuck.Boom;
+import com.example.unsuck.Null;
+
+import fj.F;
+import fj.Ord;
+import fj.P;
+import fj.P2;
+import fj.data.List;
+import fj.data.Option;
+
+/* Code can be mapped injectively to List<String> like this:
+ *    +       +     +    +      +      +     +     +     +     +    +    +    +     +    +    +
+ * A  B  C -> [     A -> [    A   B -> [    A B -> [    A B -> [    A -> [    A  -> [    A -> [
+ *            A     +    A             A      +    A    + +    A    +    A    <>    A    +    A
+ *            *     B    +             *      C    *    C D    +    C    +          <    B    +
+ *            [     +    [             [           [           [    +    [          >   <A>   [
+ *            ]     C    B             ]           ]           C    B    C          ]         B
+ *            B          +             B           B           *    +    +                    <
+ *            *          [             *           +           [    D    [                    A
+ *            [          C             [           [           ]         B                    >
+ *            ]          *             ]           C           ]         +                    ]
+ *            C          [             ]           *           B         [                    ]
+ *            *          ]                         [           +         D
+ *            [          ]                         ]           [         *
+ *            ]          ]                         ]           D         [
+ *            ]          ]                         ]           *         ]
+ *                                                             [         ]
+ *                                                             ]         ]
+ *                                                             ]         ]
+ *                                                             ]         ]
+ */
 public final class CodeOrd {
+  private static List<String> f(Code c, List<String> l) {
+    l = l.cons(c.tag.toString());
+    l = l.cons("[");
+    for (P2<Label, CodeRef> lc : c.labels) { // fuck... don't know if labels
+                                             // will be in order
+      l = l.cons(lc._1().label);
+      CodeRef cr = lc._2();
+      switch (cr.tag) {
+      case PATH:
+        l = ff(cr.path, l);
+        break;
+      case CODE:
+        l = f(c, l);
+        break;
+      default:
+        Boom.boom();
+      }
+    }
+    l = l.cons("]");
+    return l;
+  }
+
+  private static List<String> ff(List<Label> path, List<String> l) {
+    return null;
+  }
 
   public static Ord<Code> ord() {
-    return Ord.ord(new F2<Code, Code, Ordering>() {
-      @Override
-      public Ordering f(Code a, Code b) {
-        int hcA = System.identityHashCode(a);
-        int hcB = System.identityHashCode(b);
-        return hcA < hcB ? Ordering.LT
-            : (hcA > hcB ? Ordering.GT : Ordering.EQ);
-      }
-    }.curry());
+    return Ord.p2Ord(Ord.stringOrd, Ord.listOrd(Ord.stringOrd)).comap(
+        new F<Code, P2<String, List<String>>>() {
+          @Override
+          public List<String> f(Code c) {
+            Null.notNull(c);
+            return serialize(c);
+
+          };
+        });
   }
 }
