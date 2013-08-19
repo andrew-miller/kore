andrew-miller/kore

commit eda253f8a81e96cd8bc5d545b2fef18e9316db57

-replace java.util.Map with immutable persistent map based on prefix tree -change CodeEditorActivity into a View   -MainActivity had to transfer more than 1mb of data to CodeEditorActivity using an Intent, but it doesn't allow sending more than 1mb. using Views avoids serialization/IPC -new bug: race condition when selecting two codes at once from the recent codes list   -it somehow opens two code editors, one overlayed on the other   -pressing + to add a new code multiple times really fast seems to open multiple fresh code editors overlayed on each other

Kore/src/com/example/kore/ui/MainActivity.java
@@ -4,7 +4,6 @@ import static com.example.kore.utils.ListUtils.cons;
 import static com.example.kore.utils.ListUtils.nil;
 import static com.example.kore.utils.Null.notNull;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import com.example.kore.R;
 import com.example.kore.codes.CanonicalCode;
@@ -12,9 +11,7 @@ import com.example.kore.codes.Code;
 import com.example.kore.codes.Label;
 import com.example.kore.utils.CodeUtils;
 import com.example.kore.utils.List;
-import com.example.kore.utils.MapUtils;
-
-import android.content.Intent;
+import com.example.kore.utils.Map;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
@@ -23,24 +20,31 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MainActivity extends FragmentActivity implements
-    CodeList.CodeSelectListener, CodeList.CodeAliasChangedListener {
+    CodeList.CodeSelectListener, CodeList.CodeAliasChangedListener,
+    CodeEditorActivity.DoneListener {
 
   private static final String STATE_CODES = "codes";
   private static final String STATE_RECENT_CODES = "recent_codes";
   private static final String STATE_CODE_LABEL_ALIASES = "code_label_aliases";
   private static final String STATE_CODE_ALIASES = "code_aliases";
+  private static final String STATE_CODE_EDITOR = "code_editor";
 
   private HashSet<Code> codes = new HashSet<Code>();
   private List<Code> recentCodes = nil();
-  private HashMap<CanonicalCode, HashMap<Label, String>> codeLabelAliases =
-      new HashMap<CanonicalCode, HashMap<Label, String>>();
-  private HashMap<CanonicalCode, String> codeAliases =
-      new HashMap<CanonicalCode, String>();
+  private Map<CanonicalCode, Map<Label, String>> codeLabelAliases = Map.empty();
+  private Map<CanonicalCode, String> codeAliases = Map.empty();
+  private View mainLayout;
+  private ViewGroup codeEditorContainer;
+  private CodeEditorActivity codeEditor;
 
   @Override
   protected void onCreate(Bundle b) {
     super.onCreate(b);
+
     setContentView(R.layout.activity_main);
+    mainLayout = findViewById(R.id.main_layout);
+    codeEditorContainer = (ViewGroup) findViewById(R.id.container_code_editor);
+
     ((Button) findViewById(R.id.button_new_code))
         .setOnClickListener(new OnClickListener() {
           @Override
@@ -49,32 +53,25 @@ public class MainActivity extends FragmentActivity implements
           }
         });
 
+    Bundle codeEditorState = null;
     if (b != null) {
       codes = (HashSet<Code>) b.get(STATE_CODES);
       recentCodes = (List<Code>) b.get(STATE_RECENT_CODES);
       codeLabelAliases =
-          (HashMap<CanonicalCode, HashMap<Label, String>>) b
+          (Map<CanonicalCode, Map<Label, String>>) b
               .get(STATE_CODE_LABEL_ALIASES);
-      codeAliases = (HashMap<CanonicalCode, String>) b.get(STATE_CODE_ALIASES);
+      codeAliases = (Map<CanonicalCode, String>) b.get(STATE_CODE_ALIASES);
+      codeEditorState = b.getBundle(STATE_CODE_EDITOR);
     }
 
     initRecentCodes();
-  }
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (data == null)
-      return;
-    Code code =
-        (Code) data.getSerializableExtra(CodeEditorActivity.RESULT_CODE);
-    codeLabelAliases =
-        MapUtils
-            .cloneNestedMap((HashMap<CanonicalCode, HashMap<Label, String>>) data
-                .getSerializableExtra(CodeEditorActivity.RESULT_LABEL_ALIASES));
-    notNull(code, codeLabelAliases);
-    if (!codes.contains(code))
-      recentCodes = cons(code, recentCodes);
-    codes.add(code);
+    if (codeEditorState != null) {
+      codeEditor = new CodeEditorActivity(this, codeEditorState, this);
+      mainLayout.setVisibility(View.GONE);
+      codeEditorContainer.addView(codeEditor);
+      codeEditorContainer.setVisibility(View.VISIBLE);
+    }
   }
 
   @Override
@@ -94,38 +91,53 @@ public class MainActivity extends FragmentActivity implements
     b.putSerializable(STATE_RECENT_CODES, recentCodes);
     b.putSerializable(STATE_CODE_LABEL_ALIASES, codeLabelAliases);
     b.putSerializable(STATE_CODE_ALIASES, codeAliases);
+    if (codeEditor != null)
+      b.putBundle(STATE_CODE_EDITOR, codeEditor.getState());
   }
 
   private void initRecentCodes() {
     CodeList cl =
-        new CodeList(this, this, recentCodes,
-            MapUtils.cloneNestedMap(codeLabelAliases), this,
-            new HashMap<CanonicalCode, String>(codeAliases));
+        new CodeList(this, this, recentCodes, codeLabelAliases, this,
+            codeAliases);
     ViewGroup v = (ViewGroup) findViewById(R.id.container_recent_codes);
     v.removeAllViews();
     v.addView(cl);
   }
 
   private void startCodeEditor(Code c) {
-    startActivityForResult(
-        new Intent(this, CodeEditorActivity.class)
-            .putExtra(CodeEditorActivity.ARG_CODE, c)
-            .putExtra(CodeEditorActivity.ARG_CODE_LABEL_ALIASES,
-                MapUtils.cloneNestedMap(codeLabelAliases))
-            .putExtra(CodeEditorActivity.ARG_CODE_ALIASES,
-                new HashMap<CanonicalCode, String>(codeAliases))
-            .putExtra(CodeEditorActivity.ARG_CODES, recentCodes), 0);
+    codeEditor =
+        new CodeEditorActivity(this, c, codeLabelAliases, codeAliases,
+            recentCodes, this);
+    mainLayout.setVisibility(View.GONE);
+    codeEditorContainer.addView(codeEditor);
+    codeEditorContainer.setVisibility(View.VISIBLE);
   }
 
   @Override
   public void onCodeSelected(Code c) {
+    notNull(c);
     startCodeEditor(c);
   }
 
   @Override
   public void codeAliasChanged(Code code, List<Label> path, String alias) {
     notNull(code, alias);
-    codeAliases.put(new CanonicalCode(code, path), alias);
+    codeAliases = codeAliases.put(new CanonicalCode(code, path), alias);
+    initRecentCodes();
+  }
+
+  @Override
+  public void onDone(Code code,
+      Map<CanonicalCode, Map<Label, String>> codeLabelAliases) {
+    notNull(code, codeLabelAliases);
+    codeEditor = null;
+    codeEditorContainer.removeAllViews();
+    codeEditorContainer.setVisibility(View.GONE);
+    mainLayout.setVisibility(View.VISIBLE);
+    if (!codes.contains(code))
+      recentCodes = cons(code, recentCodes);
+    codes.add(code);
+    this.codeLabelAliases = codeLabelAliases;
     initRecentCodes();
   }
 

Kore/gen/com/example/kore/R.java
@@ -14,19 +14,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button_code=0x7f060006;
-        public static final int button_delete_field=0x7f060008;
-        public static final int button_done=0x7f06000a;
-        public static final int button_label=0x7f060005;
-        public static final int button_new_code=0x7f060002;
-        public static final int button_new_field=0x7f060007;
-        public static final int button_switch_code_op=0x7f060009;
+        public static final int button_code=0x7f060007;
+        public static final int button_delete_field=0x7f060009;
+        public static final int button_done=0x7f06000b;
+        public static final int button_label=0x7f060006;
+        public static final int button_new_code=0x7f060003;
+        public static final int button_new_field=0x7f060008;
+        public static final int button_switch_code_op=0x7f06000a;
         public static final int container_code_editor=0x7f060001;
-        public static final int container_recent_codes=0x7f060003;
-        public static final int fragment_path=0x7f060000;
-        public static final int layout_code_list=0x7f060004;
-        public static final int layout_fields=0x7f06000b;
-        public static final int layout_path=0x7f06000c;
+        public static final int container_path=0x7f060000;
+        public static final int container_recent_codes=0x7f060004;
+        public static final int layout_code_list=0x7f060005;
+        public static final int layout_fields=0x7f06000c;
+        public static final int layout_path=0x7f06000d;
+        public static final int main_layout=0x7f060002;
     }
     public static final class layout {
         public static final int activity_code_editor=0x7f030000;

Kore/res/layout/activity_code_editor.xml
@@ -6,11 +6,11 @@
     tools:context=".MainActivity"
     tools:ignore="MergeRootFrame" >
 
-    <fragment
-        android:id="@+id/fragment_path"
-        android:name="com.example.kore.ui.Path"
-        android:layout_width="wrap_content"
-        android:layout_height="match_parent" />
+    <FrameLayout
+        android:id="@+id/container_path"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content" >
+    </FrameLayout>
 
     <FrameLayout
         android:id="@+id/container_code_editor"

Kore/res/layout/activity_main.xml
@@ -1,20 +1,35 @@
 <?xml version="1.0" encoding="utf-8"?>
 <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    android:orientation="vertical" >
+	    android:layout_width="match_parent"
+	    android:layout_height="match_parent"
+	    android:orientation="vertical" >
 
-    <Button
-        android:id="@+id/button_new_code"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:text="@string/button_new_code" />
+	<FrameLayout
+	    android:id="@+id/container_code_editor"
+	    android:layout_width="match_parent"
+	    android:layout_height="match_parent"
+	    android:visibility="gone" >
 
-    <FrameLayout
-        android:id="@+id/container_recent_codes"
-        android:layout_width="match_parent"
-        android:layout_height="wrap_content" >
+	</FrameLayout>
 
-    </FrameLayout>
+	<LinearLayout
+	    android:id="@+id/main_layout"
+	    android:layout_width="match_parent"
+	    android:layout_height="match_parent"
+	    android:orientation="vertical" >
+
+	    <Button
+	        android:id="@+id/button_new_code"
+	        android:layout_width="wrap_content"
+	        android:layout_height="wrap_content"
+	        android:text="@string/button_new_code" />
+
+	    <FrameLayout
+	        android:id="@+id/container_recent_codes"
+	        android:layout_width="match_parent"
+	        android:layout_height="wrap_content" >
+	    </FrameLayout>
+	</LinearLayout>
 
 </LinearLayout>
+

Kore/src/com/example/kore/codes/Blob.java
@@ -1,9 +1,7 @@
 package com.example.kore.codes;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.example.kore.utils.CodeUtils;
+import com.example.kore.utils.Map;
 
 public class Blob {
   private String id;
@@ -57,9 +55,11 @@ public class Blob {
       this.args = args;
       in = CodeUtils.unit;
 
-      Map<Label, CodeOrPath> subType = new HashMap<Label, CodeOrPath>();
+      Map<Label, CodeOrPath> subType = Map.empty();
       for (int i = 0; i < order.length; i++)
-        subType.put(order[i], CodeOrPath.newCode(args.get(order[i]).out));
+        subType =
+            subType.put(order[i],
+                CodeOrPath.newCode(args.get(order[i]).some().x.out));
       out = Code.newProduct(subType);
     }
 
@@ -80,7 +80,7 @@ public class Blob {
     public Label label;
 
     public Proj(Blob arg, Label label) {
-      super(arg.in, arg.out.labels.get(label).code);
+      super(arg.in, arg.out.labels.get(label).some().x.code);
       this.arg = arg;
       this.label = label;
     }

Kore/src/com/example/kore/codes/Code.java
@@ -3,8 +3,7 @@ package com.example.kore.codes;
 import static com.example.kore.utils.Null.notNull;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
+import com.example.kore.utils.Map;
 
 public final class Code implements Serializable {
   public enum Tag {
@@ -17,7 +16,7 @@ public final class Code implements Serializable {
   public Code(Tag tag, Map<Label, CodeOrPath> labels) {
     notNull(tag, labels);
     this.tag = tag;
-    this.labels = new HashMap<Label, CodeOrPath>(labels);
+    this.labels = labels;
   }
 
   @Override

commit 0ca88bac0b54c27fdef124dfa59fd3f3e340d1f0

fix race condition causing multiple CodeEditors to be superimosed
the race condition was introduced and described in eda253f8a81e96cd8bc5d545b2fef18e9316db57

Kore/src/com/example/kore/ui/MainActivity.java
@@ -9,6 +9,7 @@ import com.example.kore.R;
 import com.example.kore.codes.CanonicalCode;
 import com.example.kore.codes.Code;
 import com.example.kore.codes.Label;
+import com.example.kore.ui.CodeEditor.DoneListener;
 import com.example.kore.utils.CodeUtils;
 import com.example.kore.utils.List;
 import com.example.kore.utils.Map;
@@ -20,8 +21,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MainActivity extends FragmentActivity implements
-    CodeList.CodeSelectListener, CodeList.CodeAliasChangedListener,
-    CodeEditor.DoneListener {
+    CodeList.CodeSelectListener, CodeList.CodeAliasChangedListener {
 
   private static final String STATE_CODES = "codes";
   private static final String STATE_RECENT_CODES = "recent_codes";
@@ -36,6 +36,7 @@ public class MainActivity extends FragmentActivity implements
   private View mainLayout;
   private ViewGroup codeEditorContainer;
   private CodeEditor codeEditor;
+  private DoneListener codeEditorDoneListener;
 
   @Override
   protected void onCreate(Bundle b) {
@@ -67,7 +68,9 @@ public class MainActivity extends FragmentActivity implements
     initRecentCodes();
 
     if (codeEditorState != null) {
-      codeEditor = new CodeEditor(this, codeEditorState, this);
+      newCodeEditorDoneListener();
+      codeEditor =
+          new CodeEditor(this, codeEditorState, codeEditorDoneListener);
       mainLayout.setVisibility(View.GONE);
       codeEditorContainer.addView(codeEditor);
       codeEditorContainer.setVisibility(View.VISIBLE);
@@ -105,12 +108,45 @@ public class MainActivity extends FragmentActivity implements
   }
 
   private void startCodeEditor(Code c) {
+    /*
+     * Workaround android behavior (can't tell if bug or feature): Without this,
+     * a user could create multiple superimposed CodeEditors. He could do this
+     * by pressing on two codes in the recent code list at the same time, or by
+     * quickly pressing the "new code" button multiple times.
+     */
+    if (codeEditorDoneListener != null)
+      return;
+
+    newCodeEditorDoneListener();
     codeEditor =
-        new CodeEditor(this, c, codeLabelAliases, codeAliases,
-            recentCodes, this);
+        new CodeEditor(this, c, codeLabelAliases, codeAliases, recentCodes,
+            codeEditorDoneListener);
     mainLayout.setVisibility(View.GONE);
     codeEditorContainer.addView(codeEditor);
     codeEditorContainer.setVisibility(View.VISIBLE);
+  }
+
+  private void newCodeEditorDoneListener() {
+    codeEditorDoneListener = new CodeEditor.DoneListener() {
+      @Override
+      public void onDone(Code code,
+          Map<CanonicalCode, Map<Label, String>> codeLabelAliases) {
+        if (this != codeEditorDoneListener)
+          throw new RuntimeException(
+              "got CodeEditor.DoneListener event from old CodeEditor");
+        notNull(code, codeLabelAliases);
+        codeEditor = null;
+        codeEditorContainer.removeAllViews();
+        codeEditorContainer.setVisibility(View.GONE);
+        mainLayout.setVisibility(View.VISIBLE);
+        if (!codes.contains(code))
+          recentCodes = cons(code, recentCodes);
+        codes.add(code);
+        MainActivity.this.codeLabelAliases = codeLabelAliases;
+        initRecentCodes();
+        codeEditorDoneListener = null;
+      }
+    };
   }
 
   @Override
@@ -123,21 +159,6 @@ public class MainActivity extends FragmentActivity implements
   public void codeAliasChanged(Code code, List<Label> path, String alias) {
     notNull(code, alias);
     codeAliases = codeAliases.put(new CanonicalCode(code, path), alias);
-    initRecentCodes();
-  }
-
-  @Override
-  public void onDone(Code code,
-      Map<CanonicalCode, Map<Label, String>> codeLabelAliases) {
-    notNull(code, codeLabelAliases);
-    codeEditor = null;
-    codeEditorContainer.removeAllViews();
-    codeEditorContainer.setVisibility(View.GONE);
-    mainLayout.setVisibility(View.VISIBLE);
-    if (!codes.contains(code))
-      recentCodes = cons(code, recentCodes);
-    codes.add(code);
-    this.codeLabelAliases = codeLabelAliases;
     initRecentCodes();
   }
 