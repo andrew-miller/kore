5831f5a04bdc3891fdf29282a87f986a4460ec6c
andrew-miller <andrew-miller>
2014-03-31 15:56:19 -0400

    update RVLabelComparer for new version of RVertex.Label

--- Kore/src/com/example/kore/codes/RVLabelComparer.java
+++ Kore/src/com/example/kore/codes/RVLabelComparer.java
@@ -2,9 +2,13 @@ package com.example.kore.codes;
 
 import com.example.kore.utils.Comparer;
 import com.example.kore.utils.Comparison;
+import com.example.kore.utils.LabelComparer;
 
 public class RVLabelComparer implements Comparer<RVertex.Label> {
   public Comparison compare(RVertex.Label a, RVertex.Label b) {
+    Comparison c = new LabelComparer().compare(a.l, b.l);
+    if (c != Comparison.EQ)
+      return c;
     return new CodeComparer().compare(a.o, b.o);
   }
 }
