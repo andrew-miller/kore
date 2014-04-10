7b12d7b15e123079fc2950b3b206226c224f6e9b
andrew-miller <andrew-miller>
2014-04-10 03:13:00 -0400

    regenerate toString/hashCode/equals for Either

--- Kore/src/com/example/kore/utils/Either.java
+++ Kore/src/com/example/kore/utils/Either.java
@@ -46,4 +46,43 @@ public final class Either<X, Y> implements Serializable {
     return new Either<X, Y>(Tag.Y, null, y);
   }
 
+  @Override
+  public String toString() {
+    return "Either [tag=" + tag + ", x=" + x + ", y=" + y + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((tag == null) ? 0 : tag.hashCode());
+    result = prime * result + ((x == null) ? 0 : x.hashCode());
+    result = prime * result + ((y == null) ? 0 : y.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Either other = (Either) obj;
+    if (tag != other.tag)
+      return false;
+    if (x == null) {
+      if (other.x != null)
+        return false;
+    } else if (!x.equals(other.x))
+      return false;
+    if (y == null) {
+      if (other.y != null)
+        return false;
+    } else if (!y.equals(other.y))
+      return false;
+    return true;
+  }
+
 }
