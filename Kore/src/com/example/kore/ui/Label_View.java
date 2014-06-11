andrew-miller/kore

commit c5473ec3c3a80d636ce2637da6625783c90a8da8

remove concept of changing a relation's type
Instead, replace the relation with an empty relation of some type.

Kore/src/com/example/kore/ui/Label_View.java
@@ -11,9 +11,9 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
 import com.example.kore.codes.CanonicalCode;
+import com.example.kore.codes.CanonicalRelation;
 import com.example.kore.codes.Label;
 import com.example.kore.codes.Relation;
-import com.example.kore.codes.Relation.Tag;
 import com.example.kore.utils.Either3;
 import com.example.kore.utils.F;
 import com.example.kore.utils.List;
@@ -24,16 +24,16 @@ import com.example.kore.utils.Unit;
 
 public class Label_View {
   interface Listener {
-    void replace(Label l);
-
-    void changeRelationType(Tag t);
+    void replace(Relation r);
   }
 
   public static View make(final Context context,
       F<Either3<Label, Integer, Unit>, View> make, Integer color,
       final Relation relation, final List<Either3<Label, Integer, Unit>> path,
       Integer aliasTextColor, final CodeLabelAliasMap codeLabelAliases,
-      final RelationViewColors relationViewColors, final Listener listener) {
+      final RelationViewColors relationViewColors,
+      final Bijection<CanonicalRelation, String> relationAliases,
+      final Listener listener) {
     final Relation r = relationAt(path, relation).some().x;
     LinearLayout ll = new LinearLayout(context);
     ll.setBackgroundColor(color);
@@ -57,17 +57,18 @@ public class Label_View {
             new CanonicalCode(r.label().o, ListUtils.<Label> nil()),
             new F<Label, Void>() {
               public Void f(Label l) {
-                listener.replace(l);
+                listener.replace(Relation.label(new Relation.Label_(l, r
+                    .label().r, r.label().o)));
                 return null;
               }
             });
-        UIUtils.addRelationTypesToMenu(context, relationViewColors, p.y,
-            new F<Relation.Tag, Unit>() {
-              public Unit f(Tag t) {
-                listener.changeRelationType(t);
+        UIUtils.addEmptyRelationsToMenu(context, relationViewColors, p.y,
+            new F<Relation, Unit>() {
+              public Unit f(Relation r) {
+                listener.replace(r);
                 return unit();
               }
-            }, relation, path);
+            }, relation, path, codeLabelAliases, relationAliases);
       }
     });
 

Kore/src/com/example/kore/ui/AbstractionView.java
@@ -13,7 +13,7 @@ import com.example.kore.codes.CanonicalRelation;
 import com.example.kore.codes.Label;
 import com.example.kore.codes.Pattern;
 import com.example.kore.codes.Relation;
-import com.example.kore.codes.Relation.Tag;
+import com.example.kore.codes.Relation.Abstraction;
 import com.example.kore.utils.Either3;
 import com.example.kore.utils.F;
 import com.example.kore.utils.List;
@@ -23,9 +23,7 @@ import com.example.kore.utils.Unit;
 
 public class AbstractionView {
   interface Listener {
-    void replace(Pattern p);
-
-    void changeRelationType(Tag t);
+    void replace(Relation r);
   }
 
   public static View make(final Context context,
@@ -36,14 +34,15 @@ public class AbstractionView {
       final RelationViewColors relationViewColors,
       final Bijection<CanonicalRelation, String> relationAliases,
       final Listener listener) {
-    Relation r = relationAt(path, relation).some().x;
+    final Relation r = relationAt(path, relation).some().x;
     final LinearLayout ll = new LinearLayout(context);
     ll.setBackgroundColor(color);
     ll.addView(PatternView.make(context, aliasTextColor,
         r.abstraction().pattern, r.abstraction().i, ListUtils.<Label> nil(),
         codeLabelAliases, new F<Pattern, Unit>() {
           public Unit f(Pattern p) {
-            listener.replace(p);
+            listener.replace(Relation.abstraction(new Abstraction(p, r
+                .abstraction().r, r.abstraction().i, r.abstraction().o)));
             return unit();
           }
         }));
@@ -52,13 +51,13 @@ public class AbstractionView {
       public void onClick(View _) {
         Pair<PopupWindow, ViewGroup> p = UIUtils.makePopupWindow(context);
         p.x.showAsDropDown(ll);
-        UIUtils.addRelationTypesToMenu(context, relationViewColors, p.y,
-            new F<Relation.Tag, Unit>() {
-              public Unit f(Tag t) {
-                listener.changeRelationType(t);
+        UIUtils.addEmptyRelationsToMenu(context, relationViewColors, p.y,
+            new F<Relation, Unit>() {
+              public Unit f(Relation r) {
+                listener.replace(r);
                 return unit();
               }
-            }, relation, path);
+            }, relation, path, codeLabelAliases, relationAliases);
       }
     });
     ll.addView(make.f(Either3.<Label, Integer, Unit> z(unit())));

Kore/src/com/example/kore/ui/CompositionView.java
@@ -11,9 +11,9 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
+import com.example.kore.codes.CanonicalRelation;
 import com.example.kore.codes.Label;
 import com.example.kore.codes.Relation;
-import com.example.kore.codes.Relation.Tag;
 import com.example.kore.utils.Either3;
 import com.example.kore.utils.F;
 import com.example.kore.utils.List;
@@ -26,14 +26,17 @@ public class CompositionView {
 
     void select();
 
-    void changeRelationType(Tag t);
+    void replace(Relation r);
   }
 
   public static View make(final Context context,
       F<Either3<Label, Integer, Unit>, View> make, DragBro dragBro,
       Integer color, Integer highlightColor, final Relation relation,
       final List<Either3<Label, Integer, Unit>> path,
-      final RelationViewColors relationViewColors, final Listener listener) {
+      final RelationViewColors relationViewColors,
+      final CodeLabelAliasMap relationAliases,
+      final Bijection<CanonicalRelation, String> codeLabelAliases,
+      final Listener listener) {
     Relation r = relationAt(path, relation).some().x;
     LinearLayout ll = new LinearLayout(context);
     ll.setBackgroundColor(color);
@@ -67,13 +70,13 @@ public class CompositionView {
         public void onClick(View _) {
           Pair<PopupWindow, ViewGroup> p = UIUtils.makePopupWindow(context);
           p.x.showAsDropDown(b);
-          UIUtils.addRelationTypesToMenu(context, relationViewColors, p.y,
-              new F<Relation.Tag, Unit>() {
-                public Unit f(Tag t) {
-                  listener.changeRelationType(t);
+          UIUtils.addEmptyRelationsToMenu(context, relationViewColors, p.y,
+              new F<Relation, Unit>() {
+                public Unit f(Relation r) {
+                  listener.replace(r);
                   return unit();
                 }
-              }, relation, path);
+              }, relation, path, relationAliases, codeLabelAliases);
         }
       });
       ll.addView(b);

Kore/src/com/example/kore/ui/ProductView.java
@@ -12,9 +12,9 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
 import com.example.kore.codes.CanonicalCode;
+import com.example.kore.codes.CanonicalRelation;
 import com.example.kore.codes.Label;
 import com.example.kore.codes.Relation;
-import com.example.kore.codes.Relation.Tag;
 import com.example.kore.utils.Either3;
 import com.example.kore.utils.F;
 import com.example.kore.utils.List;
@@ -28,9 +28,10 @@ public class ProductView {
       F<Either3<Label, Integer, Unit>, View> make, Integer color,
       Integer aliasTextColor, final Relation relation,
       final List<Either3<Label, Integer, Unit>> path,
-      CodeLabelAliasMap codeLabelAliases,
+      final CodeLabelAliasMap codeLabelAliases,
+      final Bijection<CanonicalRelation, String> relationAliases,
       final RelationViewColors relationViewColors,
-      final F<Relation.Tag, Unit> changeRelationType) {
+      final F<Relation, Unit> replace) {
     Relation r = relationAt(path, relation).some().x;
     LinearLayout ll = new LinearLayout(context);
     ll.setOrientation(LinearLayout.VERTICAL);
@@ -57,13 +58,13 @@ public class ProductView {
         public void onClick(View _) {
           Pair<PopupWindow, ViewGroup> p = UIUtils.makePopupWindow(context);
           p.x.showAsDropDown(b);
-          UIUtils.addRelationTypesToMenu(context, relationViewColors, p.y,
-              new F<Relation.Tag, Unit>() {
-                public Unit f(Tag t) {
-                  changeRelationType.f(t);
+          UIUtils.addEmptyRelationsToMenu(context, relationViewColors, p.y,
+              new F<Relation, Unit>() {
+                public Unit f(Relation r) {
+                  replace.f(r);
                   return unit();
                 }
-              }, relation, path);
+              }, relation, path, codeLabelAliases, relationAliases);
         }
       });
       ll.addView(b);

Kore/src/com/example/kore/ui/ProjectionView.java
@@ -15,10 +15,10 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
 import com.example.kore.codes.CanonicalCode;
+import com.example.kore.codes.CanonicalRelation;
 import com.example.kore.codes.Code;
 import com.example.kore.codes.Label;
 import com.example.kore.codes.Relation;
-import com.example.kore.codes.Relation.Tag;
 import com.example.kore.utils.Either3;
 import com.example.kore.utils.F;
 import com.example.kore.utils.List;
@@ -28,16 +28,16 @@ import com.example.kore.utils.Unit;
 
 public class ProjectionView {
   interface Listener {
-    void replace(List<Label> p);
-
-    void changeRelationType(Tag t);
+    void replace(Relation r);
   }
 
   public static View make(final Context context, Integer color,
       Integer aliasTextColor, final Relation relation,
       final List<Either3<Label, Integer, Unit>> path,
-      final CodeLabelAliasMap codeLabelAliases, final Code argCode,
-      final RelationViewColors relationViewColors, final Listener listener) {
+      final CodeLabelAliasMap codeLabelAliases,
+      final Bijection<CanonicalRelation, String> relationAliases,
+      final Code argCode, final RelationViewColors relationViewColors,
+      final Listener listener) {
     final Relation r = relationAt(path, relation).some().x;
     LinearLayout ll = new LinearLayout(context);
     ll.setBackgroundColor(color);
@@ -52,17 +52,18 @@ public class ProjectionView {
                 argCode, reroot(argCode, r.projection().path),
                 new F<List<Label>, Void>() {
                   public Void f(List<Label> p) {
-                    listener.replace(p);
+                    listener.replace(Relation
+                        .projection(new Relation.Projection(p, r.projection().o)));
                     return null;
                   }
                 });
-            UIUtils.addRelationTypesToMenu(context, relationViewColors, p.y,
-                new F<Relation.Tag, Unit>() {
-                  public Unit f(Tag t) {
-                    listener.changeRelationType(t);
+            UIUtils.addEmptyRelationsToMenu(context, relationViewColors, p.y,
+                new F<Relation, Unit>() {
+                  public Unit f(Relation r) {
+                    listener.replace(r);
                     return unit();
                   }
-                }, relation, path);
+                }, relation, path, codeLabelAliases, relationAliases);
           }
         });
         return unit();

Kore/src/com/example/kore/ui/RelationEditor.java
@@ -29,7 +29,6 @@ import com.example.kore.codes.Code;
 import com.example.kore.codes.Label;
 import com.example.kore.codes.Relation;
 import com.example.kore.codes.Relation.Composition;
-import com.example.kore.codes.Relation.Tag;
 import com.example.kore.utils.Either;
 import com.example.kore.utils.Either3;
 import com.example.kore.utils.F;
@@ -108,13 +107,6 @@ public class RelationEditor {
                     s.initNodeEditor.f(unit());
                     f(s.path);
                   }
-
-                  public void changeRelationType(Tag t) {
-                    s.relation =
-                        RelationUtils.changeRelationType(s.relation, s.path, t);
-                    f(s.path);
-                    s.initNodeEditor.f(unit());
-                  }
                 }, s.relation, relations, codeLabelAliases, relationAliases, p,
                 relationViewColors.referenceColors.x, relationViewColors));
             return unit();
@@ -183,14 +175,6 @@ public class RelationEditor {
                   public void selectPath(List<Either3<Label, Integer, Unit>> p) {
                     s.selectPath.f(relationOrPathAt(append(s.path, p),
                         s.relation).y());
-                  }
-
-                  public void changeRelationType(
-                      List<Either3<Label, Integer, Unit>> path, Tag t) {
-                    s.relation =
-                        RelationUtils.changeRelationType(s.relation, path, t);
-                    setPath.f(s.path);
-                    f(unit());
                   }
                 }, s.path, codes, codeLabelAliases, codeAliases,
                 relationAliases, relationViewColors);

Kore/src/com/example/kore/ui/RelationNodeEditor.java
@@ -28,7 +28,6 @@ import com.example.kore.codes.CanonicalRelation;
 import com.example.kore.codes.Code;
 import com.example.kore.codes.Label;
 import com.example.kore.codes.Relation;
-import com.example.kore.codes.Relation.Tag;
 import com.example.kore.utils.CodeUtils;
 import com.example.kore.utils.Either;
 import com.example.kore.utils.Either3;
@@ -56,8 +55,6 @@ public class RelationNodeEditor {
     void replaceRelation(List<Either3<Label, Integer, Unit>> path, Relation r);
 
     void selectPath(List<Either3<Label, Integer, Unit>> path);
-
-    void changeRelationType(List<Either3<Label, Integer, Unit>> path, Tag t);
   }
 
   class Move {
@@ -169,11 +166,6 @@ public class RelationNodeEditor {
               public boolean dontAbbreviate(
                   List<Either3<Label, Integer, Unit>> p) {
                 return p.equals(path);
-              }
-
-              public void changeRelationType(
-                  List<Either3<Label, Integer, Unit>> path, Tag t) {
-                listener.changeRelationType(path, t);
               }
             }, codeLabelAliases, relationAliases);
     // workaround that you can't drag onto the outer 10 pixels

Kore/src/com/example/kore/ui/RelationPath.java
@@ -27,7 +27,6 @@ import com.example.kore.R;
 import com.example.kore.codes.CanonicalRelation;
 import com.example.kore.codes.Label;
 import com.example.kore.codes.Relation;
-import com.example.kore.codes.Relation.Tag;
 import com.example.kore.utils.Either;
 import com.example.kore.utils.Either3;
 import com.example.kore.utils.F;
@@ -40,8 +39,6 @@ import com.example.kore.utils.Unit;
 public class RelationPath {
   interface Listener {
     void selectPath(List<Either3<Label, Integer, Unit>> p);
-
-    void changeRelationType(Tag y);
 
     void replaceRelation(
         Either<Relation, List<Either3<Label, Integer, Unit>>> er);
@@ -86,13 +83,14 @@ public class RelationPath {
                   .<Relation, List<Either3<Label, Integer, Unit>>> x(RelationUtils
                       .dummy(unit, unit)))))) {
             Pair<PopupWindow, ViewGroup> p = UIUtils.makePopupWindow(context);
-            UIUtils.addRelationTypesToMenu(context, rvc, p.y,
-                new F<Relation.Tag, Unit>() {
-                  public Unit f(Tag t) {
-                    listener.changeRelationType(t);
+            UIUtils.addEmptyRelationsToMenu(context, rvc, p.y,
+                new F<Relation, Unit>() {
+                  public Unit f(Relation r) {
+                    listener.replaceRelation(Either
+                        .<Relation, List<Either3<Label, Integer, Unit>>> x(r));
                     return unit();
                   }
-                }, root, before);
+                }, root, before, codeLabelAliases, relationAliases);
             Space s = new Space(context);
             s.setMinimumHeight(1);
             p.y.addView(s);

commit 0ea670fda9de4ae8b48524bf89957f95460f10e3

fix blindly replacing label without considering it's subrelation
regression introduced in c5473ec3c3a80d636ce2637da6625783c90a8da8

Kore/src/com/example/kore/ui/Label_View.java
@@ -1,6 +1,13 @@
 package com.example.kore.ui;
 
+import static com.example.kore.ui.RelationUtils.codomain;
+import static com.example.kore.ui.RelationUtils.defaultValue;
+import static com.example.kore.ui.RelationUtils.getRelation;
 import static com.example.kore.ui.RelationUtils.relationAt;
+import static com.example.kore.utils.CodeUtils.equal;
+import static com.example.kore.utils.CodeUtils.reroot;
+import static com.example.kore.utils.CodeUtils.unit;
+import static com.example.kore.utils.ListUtils.fromArray;
 import static com.example.kore.utils.Unit.unit;
 import android.content.Context;
 import android.view.View;
@@ -12,8 +19,11 @@ import android.widget.PopupWindow;
 
 import com.example.kore.codes.CanonicalCode;
 import com.example.kore.codes.CanonicalRelation;
+import com.example.kore.codes.Code;
 import com.example.kore.codes.Label;
 import com.example.kore.codes.Relation;
+import com.example.kore.codes.Relation.Label_;
+import com.example.kore.utils.Either;
 import com.example.kore.utils.Either3;
 import com.example.kore.utils.F;
 import com.example.kore.utils.List;
@@ -51,22 +61,29 @@ public class Label_View {
     }
     v.setOnClickListener(new OnClickListener() {
       public void onClick(View _) {
-        final Pair<PopupWindow, ViewGroup> p = UIUtils.makePopupWindow(context);
+        Pair<PopupWindow, ViewGroup> p = UIUtils.makePopupWindow(context);
         p.x.showAsDropDown(v);
         UIUtils.addRelationLabelsToMenu(p.y, context, v, codeLabelAliases,
             new CanonicalCode(r.label().o, ListUtils.<Label> nil()),
             new F<Label, Void>() {
               public Void f(Label l) {
-                p.x.dismiss();
-                listener.replace(Relation.label(new Relation.Label_(l, r
-                    .label().r, r.label().o)));
+                Relation sr =
+                    getRelation(relation, r,
+                        Either3.<Label, Integer, Unit> z(unit())).some().x;
+                Code o = reroot(r.label().o, fromArray(l));
+                listener.replace(Relation.label(new Label_(l, x(equal(
+                    codomain(sr), o) ? sr : defaultValue(unit, o)), r.label().o)));
                 return null;
+              }
+
+              private Either<Relation, List<Either3<Label, Integer, Unit>>> x(
+                  Relation relation) {
+                return Either.x(relation);
               }
             });
         UIUtils.addEmptyRelationsToMenu(context, relationViewColors, p.y,
             new F<Relation, Unit>() {
               public Unit f(Relation r) {
-                p.x.dismiss();
                 listener.replace(r);
                 return unit();
               }