125ebab8e9b8e409c816f9972d427e58974be49d
andrew-miller <andrew-miller>
2014-07-15 11:28:43 -0400

    Random: fix potential (I'm not sure what the semantics is when a statically initialized field is accessed from multiple threads) data race

--- Kore/src/com/pokemon/kore/utils/Random.java
+++ Kore/src/com/pokemon/kore/utils/Random.java
@@ -3,7 +3,7 @@ package com.pokemon.kore.utils;
 import java.security.SecureRandom;
 
 public class Random {
-  private static SecureRandom sr = new SecureRandom();
+  private static final SecureRandom sr = new SecureRandom();
 
   private static char[] m = new char[] { '0', '1', '2', '3', '4', '5', '6',
       '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
